4e642e6494babc86ca082970548e69a26a3003cd
Shubham Bhargava <86161903+shubhamgit123@users.noreply.github.com>
2022-02-21 21:21:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ a=xlrd.open_workbook(loc)
 sheet=a.sheet_by_index(0)
 sheet.cell_value(0,0)
 for i in range(5,806):
-    con=mysql.connector.connect(host="localhost",user="root",passwd="12345ss54321",database="shubhamdb1")
+    con=mysql.connector.connect(host="localhost",user="root",passwd="***********",database="shubhamdb1")
     cur=con.cursor()
     
     l.append(tuple(sheet.row_values(i)))
